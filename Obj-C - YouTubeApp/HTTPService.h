554209c25cf11db211f19011ff3c7d6b959a697a
PomHaffs <tomashaffenden@icloud.com>
2016-12-08 14:32:41 +1100

    intial commit

---
+++ Obj-C - YouTubeApp/HTTPService.h
@@ -0,0 +1,17 @@
+//
+//  HTTPService.h
+//  Obj-C - YouTubeApp
+//
+//  Created by Tomas-William Haffenden on 8/12/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+    //This is a singleton that will be used everywhere
+
+#import <Foundation/Foundation.h>
+
+@interface HTTPService : NSObject
+
++ (id) instance;
+
+@end
---
+++ Obj-C - YouTubeApp/HTTPService.m
@@ -0,0 +1,13 @@
+//
+//  HTTPService.m
+//  Obj-C - YouTubeApp
+//
+//  Created by Tomas-William Haffenden on 8/12/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+#import "HTTPService.h"
+
+@implementation HTTPService
+
+@end
